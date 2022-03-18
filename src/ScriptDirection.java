fe086e817ad085cb684969985af27f4c4549133a
jtint24 <tintjosh@gmail.com>
2022-03-18 10:22:14 -0700

    add kotlin version

--- src/ScriptDirection.java
+++
@@ -1,9 +0,0 @@
-/**
- * ScriptDirection
- *
- * enum of 3 possible script directions
- * */
-
-public enum ScriptDirection {
-  LTR, RTL, BOUSTROPHEDON
-}
--- src/SpaceProtocol.java
+++
@@ -1,9 +0,0 @@
-/**
- * SpaceProtocol
- *
- * list of 3 possible ways languages use spaces
- * */
-
-enum SpaceProtocol {
-  SPACES, NOSPACES, SYLLABIC
-}
